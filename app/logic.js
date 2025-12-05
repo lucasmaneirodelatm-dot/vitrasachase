10d7e385092f9f71ea6915fce55f4b2b6348eb77
lucasmaneirodelatm-dot <lucasmaneirodelatm@gmail.com>
2025-12-05 19:24:37 +0100

    Refactor iframe message handling and add new events

--- app/logic.js
+++ app/logic.js
@@ -1,14 +1,28 @@
+// logic.js
 const socket = io();
 
+// pasa el estado del mundo al iframe visible
 socket.on("worldUpdate", data => {
-    const frame = document.getElementById("visor");
-    if (frame)
-        frame.contentWindow.postMessage({ type: "worldUpdate", data }, "*");
+    const visor = document.getElementById("visor");
+    if (visor) visor.contentWindow.postMessage({ type: "worldUpdate", data }, "*");
 });
 
-// Mensajes desde páginas internas
+// si la sala recibe que alguien timbra → redirige a parada.html
+socket.on("descenso", data => {
+    const visor = document.getElementById("visor");
+    if (visor) {
+        visor.src = "pages/parada.html?parada=" + data.parada;
+    }
+});
+
+// mensajes desde las páginas internas
 window.addEventListener("message", ev => {
+
     if (ev.data.type === "timbreBus"){
         socket.emit("timbre", { bus: ev.data.bus });
+    }
+
+    if (ev.data.type === "askBus"){
+        socket.emit("askBus", { bus: ev.data.bus });
     }
 });
